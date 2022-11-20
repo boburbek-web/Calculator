5aee183cbfd75d9c3d9ff1205f3dd3ca39ec9ac5
Utkirov Boburbek <boburbekutkirov10@gmail.com>
2022-11-20 14:33:06 +0500

    Calculator

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function count() {
+      var num1 = document.getElementById("num1").value;
+      var num2 = document.getElementById("num2").value;
+      var mod = document.getElementById("operation").value;
+      document.getElementById('result').value = eval(num1 + mod + num2);
+    }
